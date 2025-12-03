dd4885673e0bdfcdef0b0defcbb24cf76a0e2218
tsrobinson <t.robinson7@lse.ac.uk>
2025-12-03 14:37:31 +0000

    redo part 1 and add part 2

--- day3.py
+++ day3.py
@@ -16,3 +16,35 @@ if __name__ == "__main__":
             elif int(bank[i]) > int(joltage[1]):
                 joltage = joltage[0] + bank[i]
         total_joltage += int(joltage)
+
+    # part 1 (better)
+    total_joltage = 0
+    for bank in banks:
+        bank = [int(b) for b in bank]
+        joltage = [0, 0]
+        l = 0
+        for i in range(len(bank) - 1):
+            if bank[i] > joltage[0]:
+                joltage[0] = bank[i]
+                l = i
+
+        for i in range(l + 1, len(bank)):
+            if bank[i] > joltage[1]:
+                joltage[1] = bank[i]
+
+        total_joltage += int("".join([str(x) for x in joltage]))
+
+    # part 2
+    total_joltage = 0
+    for bank in banks:
+        bank = [int(b) for b in bank]
+        joltage = [0] * 12
+        start = -1
+        for s in range(12):
+            end = len(bank) - 11 + s
+            for i in range(start + 1, end):
+                if bank[i] > joltage[s]:
+                    joltage[s] = bank[i]
+                    start = i
+
+        total_joltage += int("".join([str(x) for x in joltage]))

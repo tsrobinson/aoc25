04c815611d60cdf74593208c66cd80d772a12a41
tsrobinson <t.robinson7@lse.ac.uk>
2025-12-10 21:42:10 +0000

    day 7 part 2

--- day7.py
+++ day7.py
@@ -16,6 +16,37 @@ def descend(x, y, map):
     return splits
 
 
+def descend_quantum(x, y, grid):
+
+    if x < 0 or x >= len(grid[0]) or y < 0 or y >= len(grid):
+        return 0
+
+    # memoized result
+    if (x, y) in MEMO:
+        return MEMO[(x, y)]
+
+    if y == len(grid) - 1:
+        MEMO[(x, y)] = 1
+        return 1
+
+    cell = grid[y][x]
+
+    if cell == "S" or cell == ".":
+        # just move straight down
+        worlds = descend_quantum(x, y + 1, grid)
+
+    elif cell == "^":
+
+        worlds = descend_quantum(x - 1, y + 1, grid)
+        worlds += descend_quantum(x + 1, y + 1, grid)
+    else:
+        # unknown cell type => no paths
+        worlds = 0
+
+    MEMO[(x, y)] = worlds
+    return worlds
+
+
 if __name__ == "__main__":
     with open("data/day7.txt") as f:
         data = f.read().strip().split("\n")
@@ -24,7 +55,13 @@ if __name__ == "__main__":
     while data[0][s] == ".":
         s += 1
 
+    # part 1
     MEMO = {}
 
     total_splits = descend(s, 0, data)
     print(total_splits)
+
+    # part 2
+    MEMO = {}
+    worlds = descend_quantum(s, 0, data)
+    print(worlds)
